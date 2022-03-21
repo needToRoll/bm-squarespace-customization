a2438734a44fef1803c686554af9e539e3391dab
Florian Buergi <florian@buergi.cloud>
2022-03-21 10:44:15 +0100

    code for bungeemonkey website customizations as of 20220321

---
+++ sptfinder_cdn_module/cdn_root/gmaps_config.js
@@ -0,0 +1,43 @@
+const GOOGLE_MAPS_CONFIG = {
+    "locations": [{
+            "title": "Nydeggbrücke",
+            "address1": "Nydeggbrücke",
+            "address2": "3011 Bern, Switzerland",
+            "coords": { "lat": 46.94846104009583, "lng": 7.458410176057417 },
+            "placeId": "ChIJpwSFPtw5jkcRCjUep0M2a-U",
+            "bmSpotInfo": {
+                "difficulty": 1,
+                "ropeLength": "16 - 24m",
+                "dangers": [
+                    "Viel Schwemmholz",
+                    "Sichtbarkeit der Schwimmer vor der Kurve"
+                ]
+            }
+        },
+        {
+            "title": "Kornhausbrücke",
+            "address1": "Kornhausbrücke",
+            "address2": "3011 Bern, Switzerland",
+            "coords": { "lat": 46.9502355087515, "lng": 7.448353322090138 },
+            "placeId": "ChIJW56agcE5jkcRqoOzW9euB5c",
+            "bmSpotInfo": {
+                "difficulty": 3,
+                "ropeLength": "8 - 12m",
+                "dangers": [
+                    "Viel Schwemmholz"
+                ]
+            }
+        }
+    ],
+    "mapOptions": {
+        "center": { "lat": 38.0, "lng": -100.0 },
+        "fullscreenControl": true,
+        "mapTypeControl": false,
+        "streetViewControl": false,
+        "zoom": 9,
+        "zoomControl": true,
+        "maxZoom": 17,
+        "mapId": "ca16d2171c271f81"
+    },
+    "mapsApiKey": "AIzaSyBojiHqVA17p_BMMls8trtweQgq0Qx0pvw"
+}
---
+++ sptfinder_cdn_module/pullFromCdn.cmd
@@ -0,0 +1,1 @@
+xcopy "C:\Users\fbbue\OneDrive - Bungeemonkey\spotfinder" .\cdn_root\ /D /I /E /F /Y /H /R

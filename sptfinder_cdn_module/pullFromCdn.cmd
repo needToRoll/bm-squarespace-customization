7705aacfbc38718a2f87b2216b4ad45723b40ca5
Florian Buergi <florian@buergi.cloud>
2022-03-26 09:50:58 +0100

    restructure module

--- sptfinder_cdn_module/pullFromCdn.cmd
+++
@@ -1,1 +0,0 @@
-xcopy "C:\Users\fbbue\OneDrive - Bungeemonkey\spotfinder" .\cdn_root\ /D /I /E /F /Y /H /R
--- sptfinder_cdn_module/pushToCdn.cmd
+++
@@ -1,1 +0,0 @@
-xcopy .\cdn_root\ "C:\Users\fbbue\OneDrive - Bungeemonkey\spotfinder"  /D /I /E /F /Y /H /R
